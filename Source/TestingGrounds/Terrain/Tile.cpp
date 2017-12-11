3b7834d33a2ab8c0e66ecd110c2a75cef53a7d97
Richard Passe <richardpasse@hotmail.com>
2017-12-11 19:11:22 +0000

    Ep088 Sphere Casting in C++

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -2,6 +2,7 @@
 
 #include "TestingGrounds.h"
 #include "Tile.h"
+#include "DrawDebugHelpers.h"
 
 
 // Sets default values
@@ -32,7 +33,9 @@ void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	CastSphere(GetActorLocation(), 300);
+	CastSphere(GetActorLocation()+ FVector(0,0,1000) , 300);
+
 }
 
 // Called every frame
@@ -42,3 +45,19 @@ void ATile::Tick(float DeltaTime)
 
 }
 
+bool ATile::CastSphere(FVector Location, float Radius)
+{
+	FHitResult HitResult;
+	bool HasHit = GetWorld()->SweepSingleByChannel(
+	HitResult,
+	Location,
+	Location,
+	FQuat::Identity,
+	ECollisionChannel::ECC_Camera,
+	FCollisionShape::MakeSphere(Radius)
+	);
+
+	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
+	DrawDebugSphere(GetWorld(), Location, Radius, 16, ResultColor, true, 100);
+	return HasHit;
+}
